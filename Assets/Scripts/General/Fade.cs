afd829bd38f67d196675bc13f505b0cb7dc8e04b
Lucas Soares <lsoares267@gmail.com>
2017-01-24 15:56:16 -0200

    Upando nova versão do projeto

---
+++ Assets/Scripts/General/Fade.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour 
+{
+    private GameObject fade;
+    private byte a;
+    public string newScene;
+    public bool fadeIn;
+    public bool fadeOut;
+    void Start()
+    {
+        fadeIn = true;
+        fade = GameObject.Find("Fade");
+        a = 255;
+    }
+    void Update()//Função para atualização do alpha do fade de acordo com a booleana ativada. 
+    {
+        if (fadeIn)
+        {
+            fade.GetComponent<Renderer>().material.color = new Color32((byte)Color.black.r, (byte)Color.black.g, (byte)Color.black.b, a);
+            if (a > 0)
+            {
+                a -= 17;
+            }
+            else
+            {
+                fadeIn = false;
+                fade.SetActive(false);
+            }
+        }
+        else if (fadeOut)
+        {
+            fade.SetActive(true);
+            fade.GetComponent<Renderer>().material.color = new Color32((byte)Color.black.r, (byte)Color.black.g, (byte)Color.black.b, a);
+            if (a < 255)
+            {
+                a += 17;
+            }
+            else
+            {
+                ChangeScene();
+            }
+        }
+    }
+    void ChangeScene()//Função de controle de cenas.
+    {
+        SceneManager.LoadScene(newScene);
+    }
+    public void ToMenu()
+    {
+        fadeOut = true;
+        newScene = "Menu";
+    }
+    public void ToModes()
+    {
+        fadeOut = true;
+        newScene = "Mods";
+    }
+    public void ToAbout()
+    {
+        fadeOut = true;
+        newScene = "About";
+    }
+    public void ToProfessions()
+    {
+        fadeOut = true;
+        newScene = "Professions";
+    }
+    public void ToRules()
+    {
+        fadeOut = true;
+        newScene = "Rules";
+    }
+	public void ToAR()
+    {
+        fadeOut = true;
+        newScene = "AR";
+    }
+    public void ToDynamic()
+    {
+        fadeOut = true;
+        newScene = "Dynamic";
+    }
+}
---
+++ Assets/Scripts/Mods/Dynamic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dynamic : MonoBehaviour 
+{
+    private int time;
+    private float counter;
+    private string[] games;
+    private Text seconds, game;
+	void Start () 
+    {
+        games = new string[6];
+        games[0] = "RPG";
+        games[1] = "FPS";
+        games[2] = "Puzzle";
+        games[3] = "Simulation";
+        games[4] = "Platform";
+        games[5] = "Board Game";
+        time = Random.Range(60, 300);
+        game = GameObject.Find("Game").GetComponent<Text>();
+        game.text = games[Random.Range(0, 6)];
+        seconds = GameObject.Find("Seconds").GetComponent<Text>();
+    }
+	
+	void Update () 
+    {
+        if (time == 0)
+        {
+            game.text = games[Random.Range(0, 6)];
+            time = Random.Range(60, 300);
+        }
+        if (time > 1)
+        {
+            seconds.text = time + " segundos";
+        }
+        else
+        {
+            seconds.text = time + " segundo";
+        }
+        if (counter > 1)
+        {
+            time -= 1;
+            counter = 0;
+        }
+        else
+        {
+            counter += Time.deltaTime;
+        }
+	}
+}
